38cc043207270c7a84c89c2ccc20eb019f003d97
i-mahmud <57482101+i-mahmud@users.noreply.github.com>
2022-03-08 17:48:27 +0600

    ui

--- shopnow/src/app/app-routing.module.ts
+++ shopnow/src/app/app-routing.module.ts
@@ -6,6 +6,21 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
   },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./about/about.module').then((m) => m.AboutModule),
+  },
+  {
+    path: 'category',
+    loadChildren: () =>
+      import('./category/category.module').then((m) => m.CategoryModule),
+  },
+  {
+    path: 'contact',
+    loadChildren: () =>
+      import('./contact/contact.module').then((m) => m.ContactModule),
+  },
 ];
 
 @NgModule({
--- shopnow/src/app/category/category.module.ts
+++ shopnow/src/app/category/category.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryComponent } from './category/category.component';
-
-
+import { RouterModule, Routes } from '@angular/router';
+const routes: Routes = [
+  {
+    path: '',
+    component: CategoryComponent,
+  },
+];
 
 @NgModule({
   declarations: [CategoryComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule.forChild(routes)],
 })
-export class CategoryModule { }
+export class CategoryModule {}
