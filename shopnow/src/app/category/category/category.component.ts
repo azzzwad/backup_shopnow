4cc9b875f259a6fda670c9b04145b42e05fc2ae6
i-mahmud <57482101+i-mahmud@users.noreply.github.com>
2022-03-08 20:35:30 +0600

    kkk

--- shopnow/src/app/category/category/category.component.ts
+++ shopnow/src/app/category/category/category.component.ts
@@ -3,13 +3,47 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
+  gridview: boolean;
+  listview: boolean = true;
+  twogridview: boolean;
+  threegridview: boolean;
+  fourgridview: boolean;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  gridView() {}
+
+  listView() {
+    this.gridview = false;
+    this.twogridview = false;
+    this.threegridview = false;
+    this.fourgridview = false;
+    this.listview = true;
   }
-
+  twoGridView() {
+    this.gridview = true;
+    this.listview = false;
+    this.threegridview = false;
+    this.fourgridview = false;
+    this.twogridview = true;
+  }
+  threeGridView() {
+    this.gridview = true;
+    this.listview = false;
+    this.twogridview = false;
+    this.fourgridview = false;
+    this.threegridview = true;
+  }
+  fourGridView() {
+    this.gridview = true;
+    this.listview = false;
+    this.twogridview = false;
+    this.threegridview = false;
+    this.fourgridview = true;
+  }
 }
