456ddbbb4a2bc27cbe8df19e5031d1f6920467be
i-mahmud <57482101+i-mahmud@users.noreply.github.com>
2022-03-13 18:40:29 +0600

    cat and prod management page

---
+++ shopnow/src/app/admin/admin.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoryManagementComponent } from './category-management/category-management.component';
+
+import { UserManagementComponent } from './user-management/user-management.component';
+import { ProductManagementComponent } from './product-management/product-management.component';
+import { OrderManagementComponent } from './order-management/order-management.component';
+import { RouterModule, Routes } from '@angular/router';
+const routes: Routes = [
+  { path: 'category', component: CategoryManagementComponent },
+  { path: 'user', component: UserManagementComponent },
+  { path: 'product', component: ProductManagementComponent },
+  { path: 'order', component: OrderManagementComponent },
+];
+@NgModule({
+  declarations: [
+    CategoryManagementComponent,
+
+    UserManagementComponent,
+    ProductManagementComponent,
+    OrderManagementComponent,
+  ],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+})
+export class AdminModule {}
