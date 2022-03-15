df0bbb07b4809685f41dfd0113f7b2d6449b3162
i-mahmud <57482101+i-mahmud@users.noreply.github.com>
2022-03-15 19:56:21 +0600

    stuck with currentUser.role == 'admin'

--- shopnow/src/app/navbar/navbar.component.ts
+++ shopnow/src/app/navbar/navbar.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute } from '@angular/router';
+import { getCookie, deleteCookie } from '../helpers/cookie-utils';
+import { AuthService } from '../services/auth.service';
+import { DbService } from '../services/db.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  windowWidth: any = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  currentUser: any;
+  constructor(
+    private db: DbService,
+    private router: Router,
+    public route: ActivatedRoute,
+    private authService: AuthService
+  ) {
+    if (getCookie('_new_session')) {
+      deleteCookie('_new_session');
+      window.location.reload();
+    }
+    if (getCookie('_user_session')) {
+      this.currentUser = JSON.parse(getCookie('_user_session')!);
+    }
   }
 
+  ngOnInit(): void {
+    console.log(this.currentUser);
+  }
+  async logout() {
+    await this.authService.logout();
+    await this.router.navigate(['']);
+    window.location.reload();
+  }
 }
